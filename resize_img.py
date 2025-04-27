61d2515e2d3d7dd569becd2018d4cf6cd96e924b
Artem Moroz <elodar230101@gmail.com>
2025-04-27 12:23:54 +0200

    finished notebook 0

--- resize_img.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-
-img = cv2.imread("/Users/artemmoroz/CIIRC_work/Prague_ml/visulization_data/tang_dist_2.png")
-img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2), interpolation=cv2.INTER_AREA)
-cv2.imwrite("/Users/artemmoroz/CIIRC_work/Prague_ml/visulization_data/tang_dist_2.png", img)
-
